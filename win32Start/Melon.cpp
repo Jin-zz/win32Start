436edff5bfa4f54f74349fbf1d5a7b39d9f26796
jinzhuoqun <jzq18861817306@gmail.com>
2022-02-12 23:59:30 +0800

    have two sphere

--- win32Start/Melon.cpp
+++ win32Start/Melon.cpp
@@ -5,6 +5,7 @@
 
 
 Melon::Melon(Graphics& gfx,
+	float scaleoffset, float tx, float ty, float tz, float ang,
 	std::mt19937& rng,
 	std::uniform_real_distribution<float>& adist,
 	std::uniform_real_distribution<float>& ddist,
@@ -13,13 +14,20 @@ Melon::Melon(Graphics& gfx,
 	std::uniform_int_distribution<int>& longdist,
 	std::uniform_int_distribution<int>& latdist)
 	:
-	r(rdist(rng)),
+	// scale
+	scaleOffset(scaleoffset),
+	// translate
+	transX(tx),
+	transY(ty),
+	transZ(tz),
+	angle(ang),
+	//r(rdist(rng)),
 	droll(ddist(rng)),
-	dpitch(ddist(rng)),
-	dyaw(ddist(rng)),
-	dphi(odist(rng)),
-	dtheta(odist(rng)),
-	dchi(odist(rng)),
+	//dpitch(ddist(rng)),
+	//dyaw(ddist(rng)),
+	//dphi(odist(rng)),
+	//dtheta(odist(rng)),
+	//dchi(odist(rng)),
 	chi(adist(rng)),
 	theta(adist(rng)),
 	phi(adist(rng))
@@ -74,7 +82,7 @@ Melon::Melon(Graphics& gfx,
 	};
 	auto model = Sphere::MakeTesselated<Vertex>(latdist(rng), longdist(rng));
 	// deform vertices of model by linear transformation
-	model.Transform(dx::XMMatrixScaling(1.0f, 1.0f, 1.2f));
+	model.Transform(dx::XMMatrixScaling(1.0f, 1.0f, 1.0f));
 
 	AddBind(std::make_unique<VertexBuffer>(gfx, model.vertices));
 
@@ -86,18 +94,24 @@ Melon::Melon(Graphics& gfx,
 void Melon::Update(float dt) noexcept
 {
 	roll += droll * dt;
-	pitch += dpitch * dt;
-	yaw += dyaw * dt;
-	theta += dtheta * dt;
-	phi += dphi * dt;
-	chi += dchi * dt;
+	//pitch += dpitch * dt;
+	//yaw += dyaw * dt;
+	//theta += dtheta * dt;
+	//phi += dphi * dt;
+	//chi += dchi * dt;
 }
 
 DirectX::XMMATRIX Melon::GetTransformXM() const noexcept
 {
 	namespace dx = DirectX;
-	return dx::XMMatrixRotationRollPitchYaw(pitch, yaw, roll) *
-		dx::XMMatrixTranslation(r, 0.0f, 0.0f) *
-		dx::XMMatrixRotationRollPitchYaw(theta, phi, chi) *
-		dx::XMMatrixTranslation(0.0f, 0.0f, 20.0f);
+	dx::XMFLOAT3 vec(0.0f, 1.0f, 0.0f);
+	dx::XMVECTOR axis = dx::XMLoadFloat3(&vec);
+	
+	return dx::XMMatrixScaling(scaleOffset, scaleOffset, scaleOffset) * // Ëõ·Å¾ØÕó
+		dx::XMMatrixRotationRollPitchYaw(pitch, yaw, roll) *  // Ðý×ª¾ØÕó Å·À­½Ç
+		dx::XMMatrixRotationAxis(axis, angle) *
+		dx::XMMatrixTranslation(transX, transY, transZ) *  // Æ½ÒÆ¾ØÕó
+		//dx::XMMatrixRotationRollPitchYaw(theta, phi, chi) *
+		dx::XMMatrixTranslation(0.0f, 0.0f, 10.0f);
 }
+
--- win32Start/Melon.h
+++ win32Start/Melon.h
@@ -4,7 +4,8 @@
 class Melon : public DrawableBase<Melon>
 {
 public:
-    Melon(Graphics& gfx, std::mt19937& rng,
+    Melon(Graphics& gfx, float scaleoffset, float tx, float ty, float tz, float angle,
+        std::mt19937& rng,
         std::uniform_real_distribution<float>& adist,
         std::uniform_real_distribution<float>& ddist,
         std::uniform_real_distribution<float>& odist,
@@ -13,9 +14,17 @@ public:
         std::uniform_int_distribution<int>& latdist);
     void Update(float dt) noexcept override;
     DirectX::XMMATRIX GetTransformXM() const noexcept override;
+
 private:
+    // scale
+    float scaleOffset;
+    // translate
+    float transX;
+    float transY;
+    float transZ;
+    // rotate
+    float angle;
     // positional
-    float r;
     float roll = 0.0f;
     float pitch = 0.0f;
     float yaw = 0.0f;
